d61c33300b89fe15c88d5d465e2d4762b1cd0b1d
AhmedKamal75 <rtya89@gmail.com>
2023-11-28 19:43:08 +0200

    this character ';' can't be handled?!

--- README.md
+++ README.md
@@ -13,31 +13,4 @@ This project is a compiler built with C++17.
 2. Navigate to the project directory using `cd`.
 3. Enter the `cmake-build-debug` directory with `cd cmake-build-debug`.
 4. Build the project with the `make` command.
-5. Run the project with `./Compiler_Project`.
-
-## Project Structure
-
-The project contains the following source files:
-
-- `main.cpp`
-- `phase_one/creation/Constants.cpp`
-- `phase_one/creation/InfixToPostfix.cpp`
-- `phase_one/creation/ToAutomaton.cpp`
-- `phase_one/creation/LexicalRulesHandler.cpp`
-- `phase_one/automaton/Types.cpp`
-- `phase_one/automaton/State.cpp`
-- `phase_one/automaton/Automaton.cpp`
-- `phase_one/automaton/Utilities.cpp`
-- `phase_one/automaton/Conversions.cpp`
-
-And the following header files:
-
-- `phase_one/creation/Constants.h`
-- `phase_one/creation/InfixToPostfix.h`
-- `phase_one/creation/ToAutomaton.h`
-- `phase_one/creation/LexicalRulesHandler.h`
-- `phase_one/automaton/Types.h`
-- `phase_one/automaton/State.h`
-- `phase_one/automaton/Automaton.h`
-- `phase_one/automaton/Utilities.h`
-- `phase_one/automaton/Conversions.h`
+5. Run the project with `./Compiler_Project`, like this command `./Compiler_Project ../output/token_list.txt ../inputs/test_program.txt ../inputs/temp_file.txtls`
--- main.cpp
+++ main.cpp
@@ -14,10 +14,25 @@ LexicalRulesHandler handler;
 
 void create(const std::string &input_file_path, const std::string &data_directory_path);
 
-int main() {
-    std::string input_program_path = R"(/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/inputs/test_program.txt)";
-    std::string input_rules_path = R"(/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/inputs/temp_file.txt)";
-    std::string data_directory_path = R"(/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/data/)";
+void export_token_list_to_file(const std::vector<std::pair<std::string, std::string>> &token_list,
+                               const std::string &filename);
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <output_token_path> <input_program_path> <input_rules_path> <data_directory_path>\n";
+        return 1;
+    }
+//    std::string output_token_path = R"(../output/token_list.txt)";
+//    std::string input_program_path = R"(../inputs/test_program.txt)";
+//    std::string input_rules_path = R"(../inputs/temp_file.txt)";
+    std::string data_directory_path = R"(../data/)";
+
+    std::string output_token_path = argv[1];
+    std::string input_program_path = argv[2];
+    std::string input_rules_path = argv[3];
+//    std::string data_directory_path = argv[4];
+
 
     // create the DFA of rules and export its detains and priorities to ../data/final.txt and ../data/priorities.txt
     create(input_rules_path, data_directory_path);
@@ -30,6 +45,7 @@ int main() {
 
     // prediction
     Predictor predictor(loaded_automaton, priorities, input_program_path);
+    std::vector<std::pair<std::string, std::string >> token_list{};
     while (true) {
         std::pair<std::string, std::string> entry = predictor.get_next_token();
         if (entry.first.empty() && entry.second.empty()) {
@@ -37,9 +53,10 @@ int main() {
             break;
         }
         std::cout << entry.first << ": " << entry.second << std::endl;
+        token_list.push_back(entry);
     }
 
-
+    export_token_list_to_file(token_list, output_token_path);
     return 0;
 }
 
@@ -57,3 +74,19 @@ void create(const std::string &input_file_path, const std::string &data_director
     // export the priorities of tokens
     LexicalRulesHandler::export_priorities(handler.get_priorities(), data_directory_path + "priorities.txt");
 }
+
+
+void export_token_list_to_file(const std::vector<std::pair<std::string, std::string>> &token_list,
+                               const std::string &filename) {
+    std::ofstream outfile(filename);
+    if (!outfile) {
+        throw std::runtime_error("Failed to open file: " + filename);
+    }
+
+    for (const auto &pair: token_list) {
+        outfile << pair.first << " " << pair.second << "\n";
+    }
+
+    outfile.close();
+}
+
